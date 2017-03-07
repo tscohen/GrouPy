681b02e4e58f3aae64fbc4d9e2391e527f6fe697
Taco Cohen <taco.cohen@gmail.com>
2017-03-07 13:08:07 +0100

    Fix imports in chainer_gconv

--- groupy/gconv/chainer_gconv/__init__.py
+++ groupy/gconv/chainer_gconv/__init__.py
@@ -1,3 +1,3 @@
 
-from p4_conv import P4ConvZ2, P4ConvP4
-from p4m_conv import P4MConvZ2, P4MConvP4M
+from groupy.gconv.chainer_gconv.p4_conv import P4ConvZ2, P4ConvP4
+from groupy.gconv.chainer_gconv.p4m_conv import P4MConvZ2, P4MConvP4M
