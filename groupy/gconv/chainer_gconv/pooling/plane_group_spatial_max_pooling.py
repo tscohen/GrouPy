b72da12f3c878564cefcc3407329b5c089dc9664
Casey Li <casey@mail.cs.toronto.edu>
2018-12-17 14:05:53 -0500

    update plane_group_spatial_max_pooling.py for chainer 5.1.0

--- groupy/gconv/chainer_gconv/pooling/plane_group_spatial_max_pooling.py
+++ groupy/gconv/chainer_gconv/pooling/plane_group_spatial_max_pooling.py
@@ -1,9 +1,13 @@
 import chainer.functions as F
+from chainer import as_variable
 
 
 def plane_group_spatial_max_pooling(x, ksize, stride=None, pad=0, cover_all=True, use_cudnn=True):
+    # tuple gets passed in; wrap it in a chainer.Variable
+    x = as_variable(x)
     xs = x.data.shape
     x = F.reshape(x, (xs[0], xs[1] * xs[2], xs[3], xs[4]))
-    x = F.max_pooling_2d(x, ksize, stride, pad, cover_all, use_cudnn)
+    # chainer 5.1.0 does not use use_cudnn
+    x = F.max_pooling_2d(x, ksize, stride, pad, cover_all)
     x = F.reshape(x, (xs[0], xs[1], xs[2], x.data.shape[2], x.data.shape[3]))
     return x
