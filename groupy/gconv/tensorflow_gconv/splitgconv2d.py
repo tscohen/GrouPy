c86793223570593927c21e31261be9068fe9e89a
neel <neel>
2021-04-02 07:17:35 -0400

    Update for TF2

--- groupy/gconv/tensorflow_gconv/splitgconv2d.py
+++ groupy/gconv/tensorflow_gconv/splitgconv2d.py
@@ -12,7 +12,6 @@ def gconv2d(input, filter, strides, padding, gconv_indices, gconv_shape_info,
     Tensorflow implementation of the group convolution.
     This function has the same interface as the standard convolution nn.conv2d, except for two new parameters,
     gconv_indices and gconv_shape_info. These can be obtained from gconv2d_util(), and are described below
-
     :param input: a tensor with (batch, height, width, in channels) axes.
     :param filter: a tensor with (ksize, ksize, in channels * in transformations, out channels) axes.
       The shape for filter can be obtained from gconv2d_util().
@@ -37,7 +36,7 @@ def gconv2d(input, filter, strides, padding, gconv_indices, gconv_shape_info,
     transformed_filter = transform_filter_2d_nhwc(w=filter, flat_indices=gconv_indices, shape_info=gconv_shape_info)
 
     # Convolve input with transformed filters
-    conv = tf.nn.conv2d(input=input, filter=transformed_filter, strides=strides, padding=padding,
+    conv = tf.compat.v1.nn.conv2d(input=input, filter=transformed_filter, strides=strides, padding=padding,
                         use_cudnn_on_gpu=use_cudnn_on_gpu, data_format=data_format, name=name)
 
     return conv
@@ -50,9 +49,8 @@ def gconv2d_util(h_input, h_output, in_channels, out_channels, ksize):
       1) an array of indices used in the filter transformation step of gconv2d
       2) shape information required by gconv2d
       5) the shape of the filter tensor to be allocated and passed to gconv2d
-
     :param h_input: one of ('Z2', 'C4', 'D4'). Use 'Z2' for the first layer. Use 'C4' or 'D4' for later layers.
-    :param h_output: one of ('C4', 'D4'). What kind of transformations to use (rotations or roto-reflections).
+    :param h_output: one of ('Z2', 'C4', 'D4'). What kind of transformations to use (rotations or roto-reflections).
       The choice of h_output of one layer should equal h_input of the next layer.
     :param in_channels: the number of input channels. Note: this refers to the number of (3D) channels on the group.
     The number of 2D channels will be 1, 4, or 8 times larger, depending the value of h_input.
@@ -78,10 +76,22 @@ def gconv2d_util(h_input, h_output, in_channels, out_channels, ksize):
         gconv_indices = flatten_indices(make_d4_p4m_indices(ksize=ksize))
         nti = 8
         nto = 8
+    elif h_input == 'D4' and h_output == 'Z2':
+        gconv_indices = flatten_indices(make_d4_z2_indices(ksize=ksize))
+        nti = 8
+        nto = 1
+    elif h_input == 'C4' and h_output == 'Z2':
+        gconv_indices = flatten_indices(make_c4_z2_indices(ksize=ksize))
+        nti = 4
+        nto = 1
     else:
         raise ValueError('Unknown (h_input, h_output) pair:' + str((h_input, h_output)))
 
-    w_shape = (ksize, ksize, in_channels * nti, out_channels)
+    if h_output == 'Z2':
+        w_shape = (ksize, ksize, in_channels, out_channels)
+    else:
+        w_shape = (ksize, ksize, in_channels * nti, out_channels)
+
     gconv_shape_info = (out_channels, nto, in_channels, nti, ksize)
     return gconv_indices, gconv_shape_info, w_shape
 
@@ -94,7 +104,6 @@ def gconv2d_addbias(input, bias, nti=8):
     A G-feature map usually consists of a number (e.g. 4 or 8) adjacent channels.
     This function will add a single bias vector to a stack of feature maps that has e.g. 4 or 8 times more 2D channels
     than G-channels, by replicating the bias across adjacent groups of 2D channels.
-
     :param input: tensor of shape (n, h, w, ni * nti), where n is the batch dimension, (h, w) are the height and width,
      ni is the number of input G-channels, and nti is the number of transformations in H.
     :param bias: tensor of shape (ni,)
@@ -103,3 +112,4 @@ def gconv2d_addbias(input, bias, nti=8):
     """
     # input = tf.reshape(input, ())
     pass # TODO
+
